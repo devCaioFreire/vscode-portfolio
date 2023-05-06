f8cc76b05023e1b616cf533e50f4b0a3c2b9a802
devCaioFreire <caiohenriquefreire@icloud.com>
2023-05-06 16:03:45 -0300

    Adição do link no contact.ts

--- src/app/portfolio/contact/page.tsx
+++ src/app/portfolio/contact/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default async function Contact() {
   const gistURL =
-    "https://gist.githubusercontent.com/devCaioFreire/1414cc1fd0e7e7dd621d48dd7078af33/raw/36b435609db395e1acd169a50555f10532c62056/contact.ts";
+    "https://gist.githubusercontent.com/devCaioFreire/1414cc1fd0e7e7dd621d48dd7078af33/raw/c6d7dcb97fcb5dc4e509c7db495503fdb76b0d7d/contact.ts";
   const aboutResponse = await fetch(gistURL);
   const contact = await aboutResponse.text();
 
